e98bf1c50983742449341142d321c4211c261f1b
mojyack <mojyack@gmail.com>
2024-05-09 15:24:20 +0900

    config: format

--- src/config.hpp
+++ src/config.hpp
@@ -4,6 +4,6 @@ namespace config {
 inline auto debug_websocket       = true;
 inline auto debug_xmpp_connection = true;
 inline auto debug_conference      = true;
-inline auto debug_jingle_handler = true;
-inline auto debug_ice            = true;
+inline auto debug_jingle_handler  = true;
+inline auto debug_ice             = true;
 } // namespace config
